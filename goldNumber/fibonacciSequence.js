c4b4d431273a21b440a5c9bfa326018e4b9a895a
bsao17 <declic62@gmail.com>
2021-11-06 13:27:50 +0100

    Add fibonacciSequence.js to display the result on the console

---
+++ goldNumber/fibonacciSequence.js
@@ -0,0 +1,13 @@
+const fibonacci = (items)=>{
+    let fibonacciSequence = [0,1];
+    for(let i = 2; i < items; i++){
+        fibonacciSequence[i] = fibonacciSequence[i-1] + fibonacciSequence[i-2];
+    }
+    console.log(`Fibonacci Sequence with ${items} items`, fibonacciSequence);
+    return fibonacciSequence;
+}
+
+fibonacci(10);
+fibonacci(20);
+fibonacci(30);
+fibonacci(40);
