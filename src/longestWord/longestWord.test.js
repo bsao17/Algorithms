dec62f4d06d5dc930893afd2c1ad3e11caa974ed
Bruno <54202730+bsao17@users.noreply.github.com>
2021-11-16 09:49:30 +0100

    Update longestWord.test.js

--- src/longestWord/longestWord.test.js
+++ src/longestWord/longestWord.test.js
@@ -1,6 +1,6 @@
 /* Find the longest word in a string */
 import findLongestWordLength from "./longestWord";
 
-  test('shoFind the longest word in a stringuld ', () => {
+  test('Find the longest word in a stringuld ', () => {
     expect(findLongestWordLength("The quick brown fox jumped over the lazy dog")).toBe(6);
   })
